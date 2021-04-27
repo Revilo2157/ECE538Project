9eaadc2ec2e98fe3a7432028e23bf2cbc840ea88
Oliver Rodas <oar5@vlsi05.egr.duke.edu>
2021-04-27 16:58:15 -0400

    -m

--- bash/run4small.sh
+++ bash/run4small.sh
@@ -16,8 +16,11 @@ mkdir $outDir
 echo Starting Slack
 export patterns=200
 export time=5
+
+touch ${outDir}/slacks.txt
 for c in {10..30..5}
 do
+	echo
 	echo Doing slack of $c%
 	SLACK=$c
         export TMGN=$(awk "BEGIN {printf \"%.2f\n \", $CLK_PERIOD * $SLACK / 100}");
@@ -28,6 +31,8 @@ do
 	echo Finished PrimeTime
 	tmax -shell ./tcl/sddatpg.tcl > "${outDir}/tmax_sl${SLACK}.txt"
 	echo Finished TetraMax
+
+	cat ${outDir}/tmax_sl${SLACK}.txt | grep SDQL | column | cut -d " " -f3 | xargs echo $c 
 done
 
 echo
@@ -41,21 +46,27 @@ pt_shell -f ./tcl/PT_scriptsd.tcl > "${outDir}/pt_sl${SLACK}.txt"
 
 for patterns in {100..400..25}
 do
+	echo
 	echo Doing $patterns patterns
 
 	tmax -shell ./tcl/sddatpg.tcl > "${outDir}/tmax_pat${patterns}.txt"
 	echo Finished TetraMax
+
+	cat ${outDir}/tmax_pat${patterns}.txt | grep SDQL | column | cut -d " " -f3 | xargs echo $patterns
 done
 
 echo
 echo Starting Time
 export patterns=200
-for time in {1..10..1}
+for time in $(seq 1 1 10)
 do
+	echo
 	echo Doing $time seconds
 
 	tmax -shell ./tcl/sddatpg.tcl > "${outDir}/tmax_tm${time}.txt"
 	echo Finished TetraMax
+
+	cat ${outDir}/tmax_tm${time}.txt | grep SDQL | column | cut -d " " -f3 | xargs echo $time
 done
 
 echo Done
